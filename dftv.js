15c49b3409d3ce8f7610d7d263a530b3a59e244d
Ceili Cornelison <ceili@ceilicornelison.com>
2015-09-13 13:46:13 -0500

    Adding db support for Friends. Friends...

--- dftv.js
+++ dftv.js
@@ -1,10 +1,10 @@
+Friends = new Mongo.Collection("friends");
+
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
-    tasks: [
-      { text: "This is task 1" },
-      { text: "This is task 2" },
-      { text: "This is task 3" }
-    ]
+    friends: function () {
+      return Friends.find({});
+    }
   });
 }
