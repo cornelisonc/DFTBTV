1b13375d2285304bba07c7ec8bda1192fdbe55cb
Brenna <breadnna@gmail.com>
2015-09-13 15:46:39 -0500

    Adding items to list B]

--- dftv.js
+++ dftv.js
@@ -1,16 +1,19 @@
 Friends = new Mongo.Collection("friends");
+Items = new Mongo.Collection("items");
 
 if (Meteor.isClient) {
     // This code only runs on the client
     Template.body.helpers({
         friends: function () {
             return Friends.find({}, {sort: {name: 1}});
+        },
+        items: function () {
+            return Items.find({}, {sort: {name: 1}});
         }
     });
 
     Template.body.events({
         "submit .new-friend": function (event) {
-            console.log('event!');
             event.preventDefault();
 
             var name = event.target.name.value;
@@ -25,6 +28,19 @@ if (Meteor.isClient) {
             // Clear form
             event.target.name.value = "";
             event.target.phone.value = "";
+        },
+        "submit .new-item": function (event) {
+            event.preventDefault();
+
+            var name = event.target.name.value;
+
+            Items.insert({
+                name: name,
+                createdAt: new Date() // current time
+            });
+
+            // Clear form
+            event.target.name.value = "";
         }
 
         // new item code goes here -- be sure to
